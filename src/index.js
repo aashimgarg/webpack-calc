f9ee309e64f9f3310fdb903b3362388c4e8ac1e5
aashimgarg <18bcs001@smvdu.ac.in>
2020-07-27 21:05:00 +0530

    added style loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { run } from "./app/app";
+import "./main.scss"; 
 import { AlertService } from "./app/alert.service";
 import { ComponentService } from "./app/component.service";
 const alertService = new AlertService();
